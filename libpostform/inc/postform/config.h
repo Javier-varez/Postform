548cd47a65edc71bbec425d51f9c033bd565fc02
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-01-29 22:26:23 +0100

    Add doxigen docs to libpostform

--- libpostform/inc/postform/config.h
+++ libpostform/inc/postform/config.h
@@ -6,13 +6,26 @@
 #include "postform/utils.h"
 
 namespace Postform {
+/**
+ * @brief Postform configuration structure.
+ *
+ * An instance of the configuration must be present in the
+ * ".postform_config" with the symbol name _postform_config.
+ */
 struct Config {
   const uint32_t timestamp_frequency;
 };
 }  // namespace Postform
 
-// Use this to declare the Postform configuration in your code.
-// This is a required symbol.
+/**
+ * @brief Declares a postform configuration in your application
+ *
+ * Use this to declare the Postform configuration in your code.
+ * This is a required symbol.
+ *
+ * Multiple definitions of the postform configuration will cause a
+ * linker error.
+ */
 #define DECLARE_POSTFORM_CONFIG(content) \
   CLINKAGE __attribute__((section(".postform_config"))) \
   const Postform::Config _postform_config { content }
