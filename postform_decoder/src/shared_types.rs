7d8d8f56e3a8b6298671bb69657d2ed97beef4dc
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-02-25 00:15:49 +0100

    Use bindgen to get platform descriptors
    Extracts platform descriptors from bindgen to automatically detect the
    size of primitive data types like pointers.
    
    Currently only used to distinguish between 32-bit and 64-bit pointers.
    
    Change-Id: I18aa1bcaeaaa14da3043eddeae7aec1ac2ba8721

---
+++ postform_decoder/src/shared_types.rs
@@ -0,0 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#[deny(clippy::all)]
+
+include!(concat!(env!("OUT_DIR"), "/shared_types.rs"));
