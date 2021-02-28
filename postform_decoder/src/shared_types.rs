b7ec0c5d13d5c9cd03e55da1af9a6415e4221733
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-02-28 20:00:17 +0100

    Fix clippy lints
    Change-Id: I712f35dd9c8d785af0edb7ec54c4f62037aaf0c8

--- postform_decoder/src/shared_types.rs
+++ postform_decoder/src/shared_types.rs
@@ -2,6 +2,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-#[deny(clippy::all)]
+#![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/shared_types.rs"));
