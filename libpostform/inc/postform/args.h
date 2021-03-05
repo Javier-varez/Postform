9523c20b0f99b5279b7dfae989e3e51753908ab1
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-03-06 00:06:36 +0100

    Build arguments and pass them to the logger
    This is an effort to try and reduce code bloat due to the templates by
    isolating the duplicated code into a constexpr enabled series of
    functions during build time, while keeping the logger code free of
    templates.
    
    It gives a pretty good improvement of saving 1kB in the test app.
    It also improves runtime slightly.
    
    Before
    4730          1056     104    5890    1702 build/targets/format
    After
    3854          1056     104    5014    1396 build/targets/format
    
    Closes #52
    
    Change-Id: Id5e5f65b28f56c50abc677afdf068c3b5c73b265

---
+++ libpostform/inc/postform/args.h
@@ -0,0 +1,80 @@
+#ifndef POSTFORM_ARGS_H_
+#define POSTFORM_ARGS_H_
+
+#include <array>
+#include <postform/types.h>
+#include <type_traits>
+
+namespace Postform {
+
+class Argument {
+ public:
+  const union {
+    unsigned long long unsigned_long_long;
+    signed long long signed_long_long;
+    const char* str_ptr;
+    const void* void_ptr;
+    InternedString interned_string;
+  };
+
+  const std::size_t size = 0;
+
+  const enum class Type {
+    UNSIGNED_INTEGER,
+    SIGNED_INTEGER,
+    STRING_POINTER,
+    VOID_PTR,
+    INTERNED_STRING
+  } type;
+};
+
+template<class T, std::enable_if_t<std::is_integral_v<T> && std::is_signed_v<T>, bool> = true>
+constexpr Argument make_arg(T value) {
+  return Argument {
+    .signed_long_long = value,
+    .size = sizeof(T),
+    .type = Argument::Type::SIGNED_INTEGER
+  };
+}
+
+template<class T, std::enable_if_t<std::is_integral_v<T> && std::is_unsigned_v<T>, bool> = true>
+constexpr Argument make_arg(T value) {
+  return Argument {
+    .unsigned_long_long = value,
+    .size = sizeof(T),
+    .type = Argument::Type::UNSIGNED_INTEGER,
+  };
+}
+
+template<class T, std::enable_if_t<std::is_convertible_v<T, const char*>, bool> = true>
+constexpr Argument make_arg(T value) {
+  return Argument {
+    .str_ptr = value,
+    .type = Argument::Type::STRING_POINTER
+  };
+}
+
+template<class T, std::enable_if_t<!std::is_convertible_v<T, const char*> &&
+                                   std::is_convertible_v<T, const void*>, bool> = true>
+constexpr Argument make_arg(T value) {
+  return Argument {
+    .void_ptr = value,
+    .type = Argument::Type::VOID_PTR
+  };
+}
+
+constexpr Argument make_arg(InternedString value) {
+  return Argument {
+    .interned_string = value,
+    .type = Argument::Type::INTERNED_STRING
+  };
+}
+
+template<class... T>
+constexpr std::array<Argument, sizeof...(T)> build_args(T... args) {
+  return { make_arg(args)... };
+}
+
+}  // namespace Postform
+
+#endif  // POSTFORM_ARGS_H_
