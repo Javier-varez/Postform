9133f07d617c6b92cbb45ab6f124e5f8fe40b5d0
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-03-26 23:49:54 +0100

    Discard write result and avoid warning
    Change-Id: I01e64c458f59390796dadf3f93639433fb676358

--- libpostform/src/file_logger.cpp
+++ libpostform/src/file_logger.cpp
@@ -17,8 +17,8 @@ void FileWriter::write(const uint8_t* data, uint32_t size) {
 void FileWriter::commit() {
   if (m_logger) {
     uint32_t size = m_data.size();
-    ::write(m_fd, &size, sizeof(size));
-    ::write(m_fd, m_data.data(), m_data.size());
+    [[maybe_unused]] uint32_t written = ::write(m_fd, &size, sizeof(size));
+    written = ::write(m_fd, m_data.data(), m_data.size());
     m_logger->release();
     m_logger = nullptr;
     m_fd = -1;
